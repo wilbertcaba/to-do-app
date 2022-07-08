2962196b26273131a8e224c8501434df4a826e1b
Wilbert Caba <wilbertcaba@gmail.com>
2022-07-08 18:52:51 -0400

    Restyled type='text' input fields

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -3,7 +3,7 @@ import './Form.css';
 
 export const Form = (props) => {
     return (
-        <form className='task-form' onSubmit={props.handleSubmit}>
+        <form className='task-form btn-input-absolute' onSubmit={props.handleSubmit}>
             <input type='text' placeholder='New task' value={props.taskName} onChange={props.handleChange} />
             <input type='submit' value='Add Task' />
         </form>
